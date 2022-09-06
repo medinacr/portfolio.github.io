fa3996398131d3cf0734f585ffce7c31bf17575e
medinacr <medinacristian282@gmail.com>
2022-09-05 19:01:43 -0700

    Updates

---
+++ precache-manifest.f38d24e0db617eadeff0c2e7dbd91361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bea30664e1dc30b4ad36c9d789889d",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "c8878bdfca6da386ac12",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6b6c63ff7cb94f429e70",
+    "url": "/react-frontend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "c8878bdfca6da386ac12",
+    "url": "/react-frontend-dev-portfolio/static/js/2.edc2e0a7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.edc2e0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6c63ff7cb94f429e70",
+    "url": "/react-frontend-dev-portfolio/static/js/main.87632c07.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.61f1db74ca7f2e82a4a871235fa6766b.js"
+  "/react-frontend-dev-portfolio/precache-manifest.f38d24e0db617eadeff0c2e7dbd91361.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
